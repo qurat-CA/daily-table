05d8b601d02baa2d8b8742b2a10faf17ecfa1698
quratulainCA <quratulain@code-avenue.com>
2024-11-14 18:54:51 +0500

    addded more colors and add images

--- src/config/color/index.tsx
+++ src/config/color/index.tsx
@@ -5,6 +5,9 @@ const Colors = {
   pink: '#F15F7B',
   purple: '#AC97EA',
   text: '#000',
+  textV2: '#4A4A4A',
+  pinkV2: '#EC1A73',
+  grey: '#F6F6F6',
 };
 
 export {Colors};
--- src/config/images/index.ts
+++ src/config/images/index.ts
@@ -4,6 +4,8 @@ import HalfCircle from '../../assets/images/half-circle.svg';
 import Welcome1 from '../../assets/images/welcome-1.svg';
 import Welcome2 from '../../assets/images/welcome-2.svg';
 import Welcome3 from '../../assets/images/welcome-3.svg';
+import ParentRole from '../../assets/images/parent-role.svg';
+import ChildRole from '../../assets/images/child-role.svg';
 
 export const SVGS = {
   Logo,
@@ -12,9 +14,14 @@ export const SVGS = {
   Welcome1,
   Welcome2,
   Welcome3,
+  ParentRole,
+  ChildRole,
 };
 
 export const Images = {
   AppBackgroundImage: require('../../assets/images/dummy-bg.png'),
   HalfCircle: require('../../assets/images/half-circle.png'),
+  Welcome1: require('../../assets/images/welcome-1.png'),
+  Welcome2: require('../../assets/images/welcome-2.png'),
+  Welcome3: require('../../assets/images/welcome-3.png'),
 };
